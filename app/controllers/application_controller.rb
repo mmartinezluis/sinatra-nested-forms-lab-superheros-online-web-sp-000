85d126819fb742d15ec801e5f274c37f352e8b29
Luis Martinez <luis_mmartinez@live.com>
2021-03-07 19:43:22 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
-
+require_relative ''
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
-
+  set :views, Proc.new { File.join(root, "../views/") }
+  
+  get '/'do
+    erb :team
+  end
+      
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require_all 'app'
+#require_all 'app'
+#require_relative './app/controllers/application_controller'
+require_relative './app'
