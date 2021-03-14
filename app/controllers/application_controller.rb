29af052268f0e47b285bd476ec21b15f04840bd3
Luis Martinez <luis_mmartinez@live.com>
2021-03-13 22:57:36 -0500

    commit tree 13427065f755d486b15411499a0a36a3d5304bc4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 require 'sinatra/base'
-require_relative ''
+#require_relative ''
 class App < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   
   get '/'do
-  
+
     erb :super_hero
   end
 
   post '/teams'
     
     erb :teams
-      
+  end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all 'app'
-#require_relative './app/controllers/application_controller'
+#require_relative './app/controllers/application_controller.rb'
 # require_relative '../app'
 #Dir["/path/to/directory/*.rb"].each {|file| require file }
